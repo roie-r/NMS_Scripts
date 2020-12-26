cf09f10a4a6d392ee3271b1cfc53b68e69f18da6
roie-r <41163747+roie-r@users.noreply.github.com>
2020-12-26 17:25:25 +0200

    Add files via upload

---
+++ AMUMSS/META/EFF EXP freight warp effect reduced.lua
@@ -0,0 +1,66 @@
+--[[┎────────────────────────────────────────
+	┃ Reduce freighter warp explosion effect
+────┸────────────────────────────────────--]]
+SCALE_MULT = 0.4
+
+NMS_MOD_DEFINITION_CONTAINER = {
+	MOD_FILENAME 		= '__META reduce freighter warp boom.pak',
+	MOD_AUTHOR			= 'lMonk',
+	NMS_VERSION			= '3.13',
+	MOD_BATCHNAME		= '_META ~@~collection.pak',
+	MODIFICATIONS 		= {{
+	MBIN_CHANGE_TABLE	= {
+	{
+		MBIN_FILE_SOURCE	= 'METADATA\EFFECTS\EXPLOSIONTABLE.MBIN',
+		EXML_CHANGE_TABLE	= {
+			{
+				MATH_OPERATION 		= '*',
+				INTEGER_TO_FLOAT	= 'FORCE',
+				SPECIAL_KEY_WORDS	= {'Id', 'WARP_FRT_ATMOS'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Scale',		SCALE_MULT}
+				}
+			},
+			{
+				MATH_OPERATION 		= '*',
+				INTEGER_TO_FLOAT	= 'FORCE',
+				SPECIAL_KEY_WORDS	= {'Id', 'WARP_FRT'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Scale',		SCALE_MULT}
+				}
+			},
+			{
+				MATH_OPERATION 		= '*',
+				INTEGER_TO_FLOAT	= 'FORCE',
+				SPECIAL_KEY_WORDS	= {'Id', 'WARP_FRG'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Scale',		SCALE_MULT}
+				}
+			},
+			{
+				MATH_OPERATION 		= '*',
+				INTEGER_TO_FLOAT	= 'FORCE',
+				SPECIAL_KEY_WORDS	= {'Id', 'WARP_START'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Scale',		SCALE_MULT}
+				}
+			},
+			{
+				MATH_OPERATION 		= '*',
+				INTEGER_TO_FLOAT	= 'FORCE',
+				SPECIAL_KEY_WORDS	= {'Id', 'WARP_START_FRGT'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Scale',		SCALE_MULT}
+				}
+			},
+			{
+				MATH_OPERATION 		= '*',
+				INTEGER_TO_FLOAT	= 'FORCE',
+				SPECIAL_KEY_WORDS	= {'Id', 'WARP_ARRIVE'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Scale',		SCALE_MULT}
+				}
+			}
+		}
+	}
+}}}}
---
+++ AMUMSS/META/MIS shorter wait for bioship.lua
@@ -0,0 +1,52 @@
+--[[┎───────────────────────────────────────────────
+	┃    	ALWAYS EXCLUDED FROM BATCH
+	┠──────────────────────────────────────────
+	┃ shorter wait for bioship mission
+────┸───────────────────────────────────────────--]]
+multiplier = 0.1
+
+NMS_MOD_DEFINITION_CONTAINER = {
+	MOD_FILENAME 		= '__META shorter wait for bioship mission.pak',
+	MOD_AUTHOR			= 'lMonk',
+	NMS_VERSION			= '3.13',
+	MOD_DESCRIPTION		= 'shorter wait for bioship mission',
+	MODIFICATIONS 		= {{
+	MBIN_CHANGE_TABLE	= {
+	{
+		MBIN_FILE_SOURCE	= 'METADATA\SIMULATION\MISSIONS\SPACEPOIMISSIONTABLE.MBIN',
+		EXML_CHANGE_TABLE	= {
+			{
+				MATH_OPERATION 		= '*',
+				SPECIAL_KEY_WORDS	= {'Message', 'UI_BIO_SHIP_MISSION1_MSG3'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Time',		multiplier},
+					{'Randomness',	multiplier * 3}
+				}
+			},
+			{
+				MATH_OPERATION 		= '*',
+				SPECIAL_KEY_WORDS	= {'Message', 'UI_BIO_SHIP_MISSION2_MSG3'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Time',		multiplier},
+					{'Randomness',	multiplier * 3}
+				}
+			},
+			{
+				MATH_OPERATION 		= '*',
+				SPECIAL_KEY_WORDS	= {'Message', 'UI_BIO_SHIP_MISSION3_MSG3'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Time',		multiplier},
+					{'Randomness',	multiplier * 3}
+				}
+			},
+			{
+				MATH_OPERATION 		= '*',
+				SPECIAL_KEY_WORDS	= {'Message', 'UI_BIO_SHIP_MISSION4_MSG3'},
+				VALUE_CHANGE_TABLE 	= {
+					{'Time',		multiplier},
+					{'Randomness',	multiplier * 3}
+				}
+			}
+		}
+	}
+}}}}
