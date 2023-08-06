3cec7987ca7c645cfe037f8dd74158d927896adc
roie-r <41163747+roie-r@users.noreply.github.com>
2023-08-06 10:49:45 +0300

    add recipes function

--- TABLES/RECIPES.lua
+++ TABLES/RECIPES.lua
@@ -9,52 +9,58 @@ mod_desc = [[
 
 local new_recipes = {
 	{--	make lots of sand from ferrite
-		id		= 'RECIPE_MORESAND',
-		name	= 'UI_SANDWORD_DIET7',
-		make	= 40,
-		cook	= 'False',
-		{id='SAND1',			n=6,	tp=I_.SBT,	res=true}, -- result!
-		{id='LAND1',			n=1,	tp=I_.SBT},
-		{id='LAND1',			n=1,	tp=I_.SBT}
+		id			= 'RECIPE_MORESAND',
+		name		= 'UI_SANDWORD_DIET7',
+		make		= 40,
+		cook		= false,
+		result		= {id='SAND1', n=6, tp=I_.SBT},
+		ingredients	= {
+			{id='LAND1',		n=1,	tp=I_.SBT},
+			{id='LAND1',		n=1,	tp=I_.SBT}
+		}
 	},
 	{--	glowing pellets are good for you
-		id		= 'RECIPE_HEALTHMAJOR',
-		name	= 'RECIPE_CATALYST1_ROBOT1',
-		make	= 3,
-		cook	= 'False',
-		{id='SUPERFOOD',		n=1,	tp=I_.PRD,	res=true}, -- result!
-		{id='SENTINEL_LOOT',	n=1,	tp=I_.PRD},
-		{id='FOOD_V_ROBOT',		n=1,	tp=I_.PRD},
-		{id='STELLAR2',			n=25,	tp=I_.SBT}
+		id			= 'RECIPE_HEALTHMAJOR',
+		name		= 'RECIPE_CATALYST1_ROBOT1',
+		make		= 3,
+		result		= {id='SUPERFOOD', n=1, tp=I_.PRD},
+		ingredients	= {
+			{id='SENTINEL_LOOT',n=1,	tp=I_.PRD},
+			{id='FOOD_V_ROBOT',	n=1,	tp=I_.PRD},
+			{id='STELLAR2',		n=25,	tp=I_.SBT}
+		}
 	},
 	{--	Spawning Sac - bioship inventory
-		id   	= 'RECIPE_BIOCARGO',
-		name	= 'RECIPE_ASTEROID_MIX',
-		make	= 4,
-		cook	= 'False',
-		{id='ALIEN_INV_TOKEN',	n=1,	tp=I_.PRD,	res=true}, -- result!
-		{id='FIENDCORE', 		n=2,	tp=I_.PRD},
-		{id='SPACEGUNK2', 		n=40,	tp=I_.SBT},
-		{id='ROBOT1', 			n=80,	tp=I_.SBT}
+		id   		= 'RECIPE_BIOCARGO',
+		name		= 'RECIPE_ASTEROID_MIX',
+		make		= 4,
+		result		= {id='ALIEN_INV_TOKEN', n=1, tp=I_.PRD},
+		ingredients	= {
+			{id='FIENDCORE', 	n=2,	tp=I_.PRD},
+			{id='SPACEGUNK2', 	n=40,	tp=I_.SBT},
+			{id='ROBOT1', 		n=80,	tp=I_.SBT}
+		}
 	},
 	{--	taint mag ferrite for more taint
-		id		= 'RECIPE_TAINT3',
-		name	= 'RECIPE_ASTEROID2',
-		make	= 40,
-		cook	= 'False',
-		{id='AF_METAL',			n=3,	tp=I_.SBT,	res=true}, -- result!
-		{id='AF_METAL',			n=1,	tp=I_.SBT},
-		{id='LAND3',			n=3,	tp=I_.SBT}
+		id			= 'RECIPE_TAINT3',
+		name		= 'RECIPE_ASTEROID2',
+		make		= 40,
+		result		= {id='AF_METAL', n=3, tp=I_.SBT},
+		ingredients	= {
+			{id='AF_METAL',		n=1,	tp=I_.SBT},
+			{id='LAND3',		n=3,	tp=I_.SBT}
+		}
 	},
 	{--	quicksilver from colored sources
-		id		= 'RECIPE_GAS2HG',
-		name	= 'RECIPE_ASTEROID2',
-		make	= 10,
-		cook	= 'False',
-		{id='HEXCORE',			n=1,	tp=I_.PRD,	res=true}, -- result!
-		{id='GAS1',				n=400,	tp=I_.SBT},
-		{id='GAS2',				n=400,	tp=I_.SBT},
-		{id='GAS3',				n=400,	tp=I_.SBT}
+		id			= 'RECIPE_GAS2HG',
+		name		= 'RECIPE_ASTEROID2',
+		make		= 10,
+		result		= {id='HEXCORE', n=1, tp=I_.PRD},
+		ingredients	= {
+			{id='GAS1',			n=400,	tp=I_.SBT},
+			{id='GAS2',			n=400,	tp=I_.SBT},
+			{id='GAS3',			n=400,	tp=I_.SBT}
+		}
 	}
 }
 
